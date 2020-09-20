ddaa9757788e4b54392098204fcf7ac2f3524a0a
ggmenendez <g.gmenendez@alumnos.upm.es>
2020-09-20 19:38:17 +0200

    Update messages

--- src/commands/sputnik.js
+++ src/commands/sputnik.js
@@ -4,8 +4,8 @@ const getChochiMessage = (people, capacity) => {
   const percent = Math.floor(people/capacity*100);
   if (percent > 90) return `¿Es que la gente no se puede ir a su puta casa o qué? (${people}/${capacity})(${percent}%)`;
   if (percent > 70) return `Mira está petao, espero que baje un poco porque no pienso aguantar a tanto gilipollas (${people}/${capacity})(${percent}%)`;
-  if (percent > 50) return `Me gusta ver el vaso medio lleno, pero esta vez no es algo bueno... (${people}/${capacity})(${percent}%)`;
-  if (percent > 30) return `Aún me sobra alguien, pero hay que reconocer que ahora se tiene que estar bien allí (${people}/${capacity})(${percent}%)`;
+  if (percent > 50) return `Esto empieza a estar más lleno que Llenilandia (${people}/${capacity})(${percent}%)`;
+  if (percent > 30) return `Vale, tampoco hay mucha gente, pero me pienso quejar igual (${people}/${capacity})(${percent}%)`;
   return `¡CORRE! Ahora está perfecto, hay menos gente que en un dia de escenografía (${people}/${capacity})(${percent}%)`;
 };
 
